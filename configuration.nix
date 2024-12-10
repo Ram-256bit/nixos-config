cb4e2947fab8e280ccec2b126d9a4317f085ba47
Ram-256bit <72127084+Ram-256bit@users.noreply.github.com>
2024-12-10 19:27:49 +0530

    removed config from inputs to satisfy lsp

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,8 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+# { config, pkgs, ... }:
+{ pkgs, ... }: # removed 'config' to satisfy lsp, add it if required
 
 {
   imports =
